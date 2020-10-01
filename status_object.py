5aaf84ec1ee9bfbe3573376e3d28d546341b29e7
Mo <58116522+Metropass@users.noreply.github.com>
2020-10-01 16:45:09 -0400

    Forgot args for `error_text` and `logger_info`

--- status_object.py
+++ status_object.py
@@ -1,5 +1,5 @@
 class Status:
-    def __init__(name: str, error_class: Optional[type]=None, error: Optional[BaseException] = None, tb: Optional[traceback] = None, Optional[logger_info] = None, Optional[error_text] = None):
+    def __init__(name: str, error_class: Optional[type]=None, error: Optional[BaseException] = None, tb: Optional[traceback] = None, logger_info: Optional[logger_info] = None, error_text: Optional[error_text] = None):
         self.name = name
         self.error_class = error_class
         self.tb = tb
