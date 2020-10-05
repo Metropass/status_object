c9954caf37e9a8f25b922bde303f64c5b253600e
Mo <58116522+Metropass@users.noreply.github.com>
2020-10-04 21:14:28 -0400

    Removed __del__ and clean()

--- status_object.py
+++ status_object.py
@@ -5,20 +5,6 @@ class Status:
         self.tb = tb
         self.logger_info = logger_info
         self.error_text = error_text
-
-    def __del__:
-        self.name = None
-        self.error_class = None
-        self.tb = None
-        self.logger_info = None
-        self.error_text = None
-
-    def clean():
-        self.name = None
-        self.error_class = None
-        self.tb = None
-        self.logger_info = None
-        self.error_text = None
     
     def set_name(name):
         self.name = str(name)
