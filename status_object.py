bc60f5dc9980e4c7b9fe4fd085c148c800509e5a
Mo <58116522+Metropass@users.noreply.github.com>
2020-10-04 21:16:18 -0400

    Removed Logger Arguments and Attributes

--- status_object.py
+++ status_object.py
@@ -1,11 +1,9 @@
 class Status:
-    def __init__(name: str, error_class: Optional[type]=None, error: Optional[BaseException] = None, tb: Optional[traceback] = None, logger_info: Optional[logger_info] = None, error_text: Optional[error_text] = None):
+    def __init__(name: str, error_class: Optional[type]=None, error: Optional[BaseException] = None, tb: Optional[traceback] = None):
         self.name = name
         self.error_class = error_class
         self.tb = tb
-        self.logger_info = logger_info
-        self.error_text = error_text
-    
+ 
     def set_name(name):
         self.name = str(name)
 
