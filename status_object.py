2765ff59dda958caa63cd9d6559979d11440962e
Mo <58116522+Metropass@users.noreply.github.com>
2020-10-07 14:09:41 -0400

    Updated Object to have the imports

--- status_object.py
+++ status_object.py
@@ -1,9 +1,12 @@
+import traceback
+from typing import Optional
+
 class Status:
-    def __init__(name: str, error_class: Optional[type]=None, error: Optional[BaseException] = None, tb: Optional[traceback] = None):
+    def __init__(self,name:str = None, error_class: Optional[type] = None, tb: Optional[traceback.print_exc()] = None):
+        
         self.name = name
         self.error_class = error_class
         self.tb = tb
  
-    def set_name(name):
-        self.name = str(name)
-
+    def set_name(self,s):
+        self.name = str(s)
